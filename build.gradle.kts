ee8568fd9e5ed721ce6a2ed5c099f74f41688a8d
jungwon <mac2001035@gmail.com>
2024-10-01 02:00:16 +0900

    CHORE: lombok, logback 추가.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,18 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
+
+    // Lombok 의존성
+    compileOnly("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+
+    // (Optional) 테스트 시 Lombok을 사용하고 싶을 때
+    testCompileOnly("org.projectlombok:lombok:1.18.28")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
