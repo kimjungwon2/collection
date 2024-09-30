7b9007c2f271330a0455e8d750663c5180aa787e
jungwon <mac2001035@gmail.com>
2024-10-01 02:00:30 +0900

    FEAT: 기본적인 해쉬맵 연산

--- src/main/java/org/jungwon/hashtable/HashTableExample.java
+++ src/main/java/org/jungwon/hashtable/HashTableExample.java
@@ -1,8 +1,11 @@
 package org.jungwon.hashtable;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class HashTableExample {
 
     public void useHash(){
@@ -12,9 +15,19 @@ public class HashTableExample {
         hashtable.put(3, "three");
 
         String three = hashtable.get(3);
+        log.info("three:{}", three);
 
         hashtable.replace(3,"삼");
+        log.info("hashtable:{}", hashtable);
 
+        hashtable.remove(2);
+        log.info("hashtable:{}", hashtable);
+
+        if (hashtable.containsKey(2)) {
+            log.info("2 : {}",hashtable.get(2));
+        } else {
+            log.info("저장된 키가 없습니다.");
+        }
     }
 
 }
